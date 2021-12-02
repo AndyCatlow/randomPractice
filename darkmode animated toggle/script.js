17cc05557220a778e0e49151c3e78f63eda1b949
AndyCatlow <mrcatlow@gmail.com>
2021-12-02 22:39:17 +0200

    addjust dark toggle to christmas toggle

---
+++ darkmode animated toggle/script.js
@@ -0,0 +1,16 @@
+const sunMoonContainer = document.querySelector(".sun-moon-container");
+const backgroundVideo = document.querySelector("#backgroundVideo");
+const body = document.body;
+
+document.querySelector(".theme-toggle-button").addEventListener("click", () => {
+  body.classList.toggle("dark");
+  const currentRotation = parseInt(
+    getComputedStyle(sunMoonContainer).getPropertyValue("--rotation")
+  );
+  sunMoonContainer.style.setProperty("--rotation", currentRotation + 180);
+  if (body.classList.contains("dark")) {
+    backgroundVideo.setAttribute("src", "Snowflakes.mp4");
+  } else {
+    backgroundVideo.setAttribute("src", "ChristmasTree.mp4");
+  }
+});
